8b21ec624914573d5d432d62ef7a4fb8828235dc
BaconPopCap <Zachary.Mark.Harrison@gmail.com>
2023-12-04 20:24:33 -0700

    Fixing attack periods

--- JavaScript/timedAttackerLimited.js
+++ JavaScript/timedAttackerLimited.js
@@ -21,11 +21,11 @@ const CURRENCIES = ['EUR', 'USD', 'JPY', 'GBP', 'TRY', 'CAD'];
 // ];
 const ATTACKS = [ 
     { start: "2023-11-13T23:00:00Z", end: "2023-11-14T00:59:00Z", task: index },
-    { start: "2023-11-14T02:00:00Z", end: "2023-11-14T03:59:00Z", task: viewCart },
-    { start: "2023-11-14T05:00:00Z", end: "2023-11-14T06:59:00Z", task: setCurrency },
-    { start: "2023-11-14T08:00:00Z", end: "2023-11-14T09:59:00Z", task: browseProduct },
-    { start: "2023-11-14T11:00:00Z", end: "2023-11-14T12:59:00Z", task: addToCart },
-    { start: "2023-11-14T14:00:00Z", end: "2023-11-14T15:59:00Z", task: checkout },
+    { start: "2023-11-14T02:00:00Z", end: "2023-11-14T02:59:00Z", task: viewCart },
+    { start: "2023-11-14T05:00:00Z", end: "2023-11-14T05:59:00Z", task: setCurrency },
+    { start: "2023-11-14T08:00:00Z", end: "2023-11-14T08:59:00Z", task: browseProduct },
+    { start: "2023-11-14T11:00:00Z", end: "2023-11-14T11:59:00Z", task: addToCart },
+    { start: "2023-11-14T14:00:00Z", end: "2023-11-14T14:59:00Z", task: checkout },
   ];
 
 // ==================================
