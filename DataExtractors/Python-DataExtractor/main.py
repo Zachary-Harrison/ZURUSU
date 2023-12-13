2df5c4f74b892b259e292568ee6cfa3a8fcb1e84
BaconPopCap <Zachary.Mark.Harrison@gmail.com>
2023-12-12 17:16:53 -0700

    Updating timedLimitedAttacker.js

--- DataExtractors/Python-DataExtractor/main.py
+++ DataExtractors/Python-DataExtractor/main.py
@@ -8,8 +8,8 @@ from datetime import datetime, timezone
 def main():
     interval = monitoring_v3.TimeInterval(
             {
-                "start_time": "2023-11-14T19:00:00Z",
-                "end_time": "2023-11-27T19:00:00Z",
+                "start_time": "2023-12-13T01:00:00Z",
+                "end_time": "2023-12-13T21:00:00Z",
             }
         )
     
@@ -24,18 +24,18 @@ def main():
         )
     
     attack_periods = [
-        ("2023-12-10T03:00:00Z", "2023-12-10T03:59:00Z"),
-        ("2023-12-10T06:00:00Z", "2023-12-10T06:59:00Z"),
-        ("2023-12-10T09:00:00Z", "2023-12-10T09:59:00Z"),
-        ("2023-12-10T12:00:00Z", "2023-12-10T12:59:00Z"),
-        ("2023-12-10T15:00:00Z", "2023-12-10T15:59:00Z"),
-        ("2023-12-10T18:00:00Z", "2023-12-10T18:59:00Z"),
+        ("2023-12-13T03:00:00Z", "2023-12-13T03:59:00Z"),
+        ("2023-12-13T06:00:00Z", "2023-12-13T06:59:00Z"),
+        ("2023-12-13T09:00:00Z", "2023-12-13T09:59:00Z"),
+        ("2023-12-13T12:00:00Z", "2023-12-13T12:59:00Z"),
+        ("2023-12-13T15:00:00Z", "2023-12-13T15:59:00Z"),
+        ("2023-12-13T18:00:00Z", "2023-12-13T18:59:00Z"),
     ]
 
     # converting attack_periods into posixTimes
     attack_periods = [(datetime.strptime(start, "%Y-%m-%dT%H:%M:%SZ"), datetime.strptime(end, "%Y-%m-%dT%H:%M:%SZ")) for start, end in attack_periods]
     attack_periods = [(start.replace(tzinfo=timezone.utc).timestamp(), end.replace(tzinfo=timezone.utc).timestamp()) for start, end in attack_periods]
-    
+
     filter = "metric.type=\"kubernetes.io/container/cpu/core_usage_time\" \
                         metadata.system_labels.\"service_name\"!=monitoring.regex.full_match(\"default-http-backend\") \
                         metadata.system_labels.\"service_name\"!=monitoring.regex.full_match(\"frontend-external\") \
